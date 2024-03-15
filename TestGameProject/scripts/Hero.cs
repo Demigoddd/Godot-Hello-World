d581b9a124dee1b4cabda8c5bcbcdcaa634658f3
Demigoddd <armen1298@gmail.com>
2024-03-15 17:27:31 +0400

    Update Hero scripts

--- TestGameProject/scripts/Hero.cs
+++ TestGameProject/scripts/Hero.cs
@@ -5,14 +5,12 @@ public partial class Hero : CharacterBody2D
 {
 	private int _speed = 400;
 	private float _angularSpeed = Mathf.Pi;
+	private Vector2 heroIconNewScale = new Vector2((float)0.2, (float)0.2);
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("Hello World");
-
-		var timer = GetNode<Timer>("Timer");
-		timer.Timeout += OnTimerTimeout;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -50,13 +48,35 @@ public partial class Hero : CharacterBody2D
 	private void OnButtonPressed()
 	{
 		SetProcess(!IsProcessing());
+
+		var heroTimer = GetNode<Timer>("HeroTimer");
+		if (IsProcessing())
+		{
+			heroTimer.Start();
+		}
+		else
+		{
+			heroTimer.Stop();
+		}
 	}
 
-	private void OnTimerTimeout()
+	private void onTimerTimeout()
 	{
-		// Visible = !Visible;
+		var heroIcon = GetNode<Node2D>("HeroIcon");
+		var heroCollision = GetNode<CollisionShape2D>("HeroCollisionShape");
 
-		var sprite = GetNode<Sprite2D>("Hero");
-		// sprite.Transform.Scale = Vector2(1.5, 1.5);
+		if (heroIcon.Scale >= new Vector2(2,2))
+		{
+			heroIcon.Scale -= new Vector2(1, 1);
+			heroCollision.Scale -= new Vector2(1, 1);
+		}
+		else
+		{
+			heroIcon.Scale += new Vector2(1, 1);
+			heroCollision.Scale += new Vector2(1, 1);
+		}
+
+		GD.Print(heroIcon.Scale);
+		GD.Print(heroCollision.Scale);
 	}
 }
